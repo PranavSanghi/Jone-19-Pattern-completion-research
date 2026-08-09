5c37e655653615f067c407034188ad667898a529
PranavSanghi <70435115+PranavSanghi@users.noreply.github.com>
2026-08-09 09:12:45 +0530

    Swapped to renset50 , added dropout

--- Modelling/model1.py
+++ Modelling/model1.py
@@ -6,13 +6,16 @@ from torchvision import models
 class model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.res = models.resnet18(weights="DEFAULT")
+        self.res = models.resnet50(weights="DEFAULT")
         self.res.fc = nn.Identity()
         self.head = nn.Sequential(
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 1),
+        nn.Linear(2048, 512),
+        nn.ReLU(),
+        nn.Dropout(0.3),
+        nn.Linear(512, 1),
         )
+        for param in list(self.res.parameters())[:-20]:
+         param.requires_grad = False
 
     def forward(self, img):
         img = nn.functional.interpolate(img, size=(224, 224), mode="bilinear")
